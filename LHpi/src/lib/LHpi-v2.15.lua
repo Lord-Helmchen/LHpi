dfef8a959f2917ccb0f2159185f0973fb7f07c01
lordhelmchen <lordhelmchen@bucephalos.localdomain>
2015-07-15 11:05:02 +0200

    tcgplayer test
    tcgplayer:
    new features from template/mkm branch
    site.BuildUrl supports multiple urls per set
    
    dummy:
    improved error handling in ma.PutFile
    ma.GetUrl now uses luasockets instead of returning nil
    dummy.CompareSiteSets outputs sorted
    
    template:
    stub site.BuildUrl supports multiple urls per set
    
    lib:
    STRICTOBJTYPE default true

--- LHpi/src/lib/LHpi-v2.15.lua
+++ LHpi/src/lib/LHpi-v2.15.lua
@@ -160,6 +160,10 @@ function LHpi.Initialize()
 		---@field [parent=#global] DEBUGVARIANTS
 		DEBUGVARIANTS = false
 	end
+	if STRICTOBJTYPE==nil then
+		---@field [parent=#global] STRICTOBJTYPE
+		STRICTOBJTYPE = true
+	end
 	if OFFLINE==nil then
 		---@field [parent=#global] OFFLINE
 		OFFLINE = false
